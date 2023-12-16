2d9dff73c7ce9774999e4341d756352a8447122b
iandoesallthethings <iandoesallthethings@gmail.com>
2023-12-16 09:57:53 -0500

    First draft day 4.

---
+++ src/lib/Days/Day4.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	interface DataPoint {
+		heartRate: number
+		time: Date
+	}
+
+	let running = $state(true)
+	let heartRate = $state(0)
+	let history = $state<DataPoint[]>([])
+
+	onMount(run)
+
+	async function run() {
+		if (running) heartRate = await fetchData()
+
+		setTimeout(run, 1000)
+	}
+
+	async function fetchData() {
+		const res = await fetch('https://advent.sveltesociety.dev/data/2023/day-four.json')
+		const { heartRate } = await res.json()
+
+		history.unshift({ heartRate, time: new Date() })
+
+		return heartRate
+	}
+
+	function interval(numberOfMinutes: number) {
+		const now = new Date()
+		const start = new Date(now.getTime() - numberOfMinutes * 60 * 1000)
+
+		const dataPoints = history.filter(({ time }) => time > start)
+
+		return average(dataPoints)
+	}
+
+	function average(dataPoints: DataPoint[]) {
+		const sum = dataPoints.reduce((acc, { heartRate }) => acc + heartRate, 0)
+		return sum / dataPoints.length
+	}
+</script>
+
+<button on:click={() => (running = !running)}>
+	{running ? 'Stop' : 'Start'}
+</button>
+
+<div class="divide-y rounded border">
+	<h3>
+		Current: {heartRate}
+	</h3>
+
+	<div>10 seconds: {interval(1 / 60)}</div>
+	<div>Past 1 min: {interval(1)}</div>
+	<div>Past 10 min: {interval(10)}</div>
+	<div>Past Hour: {interval(60)}</div>
+	<div>Past Day: {interval(60 * 24)}</div>
+	<div>All Time: {average(history)}</div>
+</div>
+
+<div class="h-64 overflow-y-auto rounded border">
+	{#each history as { heartRate, time }, _index}
+		<div>{heartRate} @ {time.toLocaleTimeString()}</div>
+	{/each}
+</div>
--- src/lib/Days/index.ts
+++ src/lib/Days/index.ts
@@ -1,5 +1,6 @@
 import Day1 from './Day1.svelte'
 import Day2 from './Day2.svelte'
 import Day3 from './Day3.svelte'
+import Day4 from './Day4.svelte'
 
-export default [undefined, Day1, Day2, Day3]
+export default [undefined, Day1, Day2, Day3, Day4]
