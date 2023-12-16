0fb3f81dce8a189136dfcd4a0e04b21e433df6e5
iandoesallthethings <iandoesallthethings@gmail.com>
2023-12-16 11:13:44 -0500

    Start on Day 5.

---
+++ src/lib/Days/Day5.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	type TaskType = 'CREATED_TOY' | 'WRAPPED_PRESENT'
+
+	interface Task {
+		elf: string
+		task: TaskType
+		minutesTaken: number
+		date: string
+	}
+
+	let running = $state(true)
+	let data = $state<Task[]>([])
+
+	onMount(run)
+
+	async function run() {
+		if (running) data = await fetchData()
+
+		setTimeout(run, 1000)
+	}
+
+	async function fetchData() {
+		const res = await fetch('https://advent.sveltesociety.dev/data/2023/day-five.json')
+		return await res.json()
+	}
+
+	const relativeTime = new Intl.RelativeTimeFormat('en', { numeric: 'auto' })
+
+	function formatDate(dateString: string) {
+		const days = 1000 * 60 * 60 * 24
+		const daysSince = Math.round((new Date(dateString).getTime() - new Date().getTime()) / days)
+		return relativeTime.format(daysSince, 'days')
+	}
+</script>
+
+<button on:click={() => (running = !running)}>
+	{running ? 'Stop' : 'Start'}
+</button>
+
+<table>
+	<thead>
+		<tr>
+			<th>Elf</th>
+			<th>Task</th>
+			<th>Time (min)</th>
+			<th>Date</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each data as { elf, task, minutesTaken, date }}
+			<tr>
+				<td>{elf}</td>
+				<td>{task}</td>
+				<td>{minutesTaken}</td>
+				<td>{new Date(date).toLocaleDateString()} (~{formatDate(date)})</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
--- src/lib/Days/index.ts
+++ src/lib/Days/index.ts
@@ -2,5 +2,6 @@ import Day1 from './Day1.svelte'
 import Day2 from './Day2.svelte'
 import Day3 from './Day3.svelte'
 import Day4 from './Day4.svelte'
+import Day5 from './Day5.svelte'
 
-export default [undefined, Day1, Day2, Day3, Day4]
+export default [undefined, Day1, Day2, Day3, Day4, Day5]
