dcf96aa8babede87b8f4f38f4b378edd415d35cf
iandoesallthethings <iandoesallthethings@gmail.com>
2023-12-16 20:27:14 -0500

    Mvp day 7.

---
+++ src/lib/Days/Day7.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { morseCode, type MorseLetter } from '$lib/morse'
+	const helloWorld = `.... . .-.. .-.. --- / .-- --- .-. .-.. -..`
+
+	let input = $state(helloWorld)
+	let output = $state(`hello world`)
+
+	function translate(morse: string) {
+		const characters = morse.split(' ') as MorseLetter[]
+		return characters.map((code) => morseCode[code]).join('')
+	}
+</script>
+
+<div class="h-20 rounded bg-white/10 p-2">{output}</div>
+<input on:input={() => (output = translate(input))} type="text" bind:value={input} />
--- src/lib/Days/index.ts
+++ src/lib/Days/index.ts
@@ -4,5 +4,6 @@ import Day3 from './Day3.svelte'
 import Day4 from './Day4.svelte'
 import Day5 from './Day5.svelte'
 import Day6 from './Day6.svelte'
+import Day7 from './Day7.svelte'
 
-export default [undefined, Day1, Day2, Day3, Day4, Day5, Day6]
+export default [undefined, Day1, Day2, Day3, Day4, Day5, Day6, Day7]
